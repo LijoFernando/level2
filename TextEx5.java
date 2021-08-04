9ab79183eb561a713f798493ed4057c0b1ece038
LijoFernando <rotricklijo@gmail.com>
2021-08-04 23:08:06 +0530

    add 5progam

--- TextEx5.java
+++ TextEx5.java
@@ -3,47 +3,47 @@ import java.util.*;
 public class TextEx5 {
 
 	
-	       int c=0;
-	      void print()
+	       int childCount=0;
+	      void printCount()
 	      {
-	          System.out.println(c);
+	          System.out.println(childCount);
 	      }
-	       void count(String f[],String k)
+	       void count(String fatherArray[],String findFather)
 	      { 
-	          for(int i=0;i<f.length;i++)
+	          for(int i=0;i<fatherArray.length;i++)
 	          {
-	              if(k.equals(f[i]))
-	                  c++;
+	              if(findFather.equals(fatherArray[i]))
+	                  childCount++;
 	          }
 	      }
-	      void check(String c[],String f[],String r)
-	      {  int k=0;
-	          for(int i=0;i<f.length;i++)
+	      void checkChild(String childArray[],String fatherArray[],String data)
+	      {  int fatherCount=0;
+	          for(int i=0;i<fatherArray.length;i++)
 	          {
-	              if(r.equals(f[i]))
-	              {   k=1;
-	                  count(f,c[i]);
+	              if(data.equals(fatherArray[i]))
+	              {   fatherCount=1;
+	                  count(fatherArray,childArray[i]);
 	              }
 	          }
 	      }
 		
 	      public static void main(String args[])
 	      {
-	          Scanner s=new Scanner(System.in);
-	          TextEx5 ob=new TextEx5();
-	          int n=s.nextInt();
-	          String c[]=new String[n];
-	          String f[]=new String[n];
+	          Scanner input=new Scanner(System.in);
+	          TextEx5 obj=new TextEx5();
+	          int n=input.nextInt();
+	          String childArray[]=new String[n];
+	          String fatherArray[]=new String[n];
 	          for(int i=0;i<n;i++)
 	          {
 	             
-	              c[i]=s.next();
-	              f[i]=s.next();
-	          }s.nextLine();
-	                  String r="";
-	                  r=s.nextLine();
-	          ob.check(c, f, r);
-	          ob.print();
+	              childArray[i]=input.next();
+	              fatherArray[i]=input.next();
+	          }input.nextLine();
+	                  String data="";
+	                  data=input.nextLine();
+	          obj.checkChild(childArray, fatherArray, data);
+	          obj.printCount();
 	      }
 	   
 	}
